fee51cb77433b05122d43035b7c2442feb6f0f75
60rzvvbj <1808078515@qq.com>
2021-05-21 14:46:19 +0800

    update Test.java

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -2,6 +2,6 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world 刘秋莲大傻逼");
+        System.out.println("hello world");
     }
 }
