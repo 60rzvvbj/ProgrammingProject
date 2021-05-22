06a8ca24e450a11a604d1969faaaa1c395f30861
lijing <2549440047@qq.com>
2021-05-22 14:28:31 +0800

    5/22 14:27

--- src/main/java/pojo/PairingRequest.java
+++ src/main/java/pojo/PairingRequest.java
@@ -1,4 +1,44 @@
 package pojo;
+/**
+ * <p><b>类名：</b>{@code PairingRequest}</p>
+ * <p><b>功能：</b></p><br>配对的java bean
+ * <p><b>方法：</b></p>
+ *
+ * @author 24LJ
+ * @date 2021/5/22
+ */
+import java.util.Date;
 
 public class PairingRequest {
+    private int studentNumber;
+    private String request;
+    private Date startTime;
+    private int recipientNumber;
+    public PairingRequest(){}
+    //创建一个两个参数的构造方法，获取学生的学号和发起配对的时间
+    public PairingRequest(int studentNumber,Date startTime){
+        this.studentNumber=studentNumber;
+        this.startTime=startTime;
+    }
+    public int getStudentNumber(){
+        return studentNumber;
+    }
+    public int getRecipientNumber(){
+        return recipientNumber;
+    }
+    public Date getStartTime(){
+        return startTime;
+    }
+    public String getRequest(){
+        return request;
+    }
+    @Override
+    public String toString(){
+        return "ParingRequest{" +
+                "studentNumber='" + studentNumber + '\'' +
+                ", request='" + request + '\''+
+                "startTime='" + startTime + '\'' +
+                "recipientNumber ='" + recipientNumber + '\''+
+                '}';
+    }
 }
--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -1,4 +1,38 @@
 package pojo;
+/**
+ * <p><b>类名：</b>{@code User}</p>
+ * <p><b>功能：</b></p><br>用户的java bean
+ * <p><b>方法：</b></p>
+ *
+ * @author 24LJ
+ * @date 2021/5/22
+ */
+import java.util.Date;
 
 public class User {
+    private String username;
+    private String password;
+    private String sex;
+    private int studentNumber;
+    public int getStudentNumber(){
+        return studentNumber;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getSex(){
+        return sex;
+    }
+    @Override
+    public String toString(){
+        return "User{" +
+                "studentNumber='" + studentNumber + '\'' +
+                ", username='" + username + '\''+
+                "password='" + password + '\'' +
+                "sex ='" + sex + '\''+
+                '}';
+    }
 }
