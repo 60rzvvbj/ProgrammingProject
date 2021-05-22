056dd4124135908494c7011dcd0f8f071683ad4d
60rzvvbj <1808078515@qq.com>
2021-05-23 00:06:07 +0800

    添加了jdbc相关

---
+++ src/main/java/util/JDBCUtil.java
@@ -0,0 +1,69 @@
+package util;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCUtil {
+
+    private static DataSource ds;
+
+    static {
+        Properties pro = new Properties();
+        InputStream is = JDBCUtil.class.getClassLoader().getResourceAsStream("druid.properties");
+        try {
+            pro.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            ds = DruidDataSourceFactory.createDataSource(pro);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void close(Statement statement, Connection connection) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(ResultSet resultSet, Statement statement, Connection connection) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        close(statement, connection);
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+import util.JDBCUtil;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(JDBCUtil.getConnection());
+    }
+}
