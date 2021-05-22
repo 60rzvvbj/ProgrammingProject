c8512cbd62c58d96879578ec95e58c0e70d8e807
60rzvvbj <1808078515@qq.com>
2021-05-22 23:41:19 +0800

    新增了Dao工厂类

--- src/main/java/commom/factory/DaoFactory.java
+++ src/main/java/commom/factory/DaoFactory.java
@@ -1,4 +1,26 @@
 package commom.factory;
 
+import dao.*;
+import dao.impl.*;
+
 public class DaoFactory {
+    public static AdministratorDao getAdministratorDao() {
+        return new AdministratorDaoImpl();
+    }
+
+    public static FeedbackDao getFeedbackDao() {
+        return new FeedbackDaoImpl();
+    }
+
+    public static FriendRequestDao getFriendRequestDao() {
+        return new FriendRequestDaoImpl();
+    }
+
+    public static PairingRequestDao getPairingRequestDao() {
+        return new PairingRequestDaoImpl();
+    }
+
+    public static UserDao getUserDao() {
+        return new UserDaoImpl();
+    }
 }
