d0426d459c9417723a56c61b47cafab2ffb85489
60rzvvbj <1808078515@qq.com>
2021-06-06 17:50:33 +0800

    修改了Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,21 @@ import commom.factory.ServiceFactory;
 import dao.AdministratorDao;
 import dao.UserDao;
 import dao.impl.UserDaoImpl;
+import service.FriendRequestService;
+import service.InformationService;
 import service.UserService;
 import util.JsonUtil;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        UserService userService = ServiceFactory.getUserService();
-        UserDao userDao = DaoFactory.getUserDao();
-        System.out.println(userDao.queryAllUser());
-
+//        UserService userService = ServiceFactory.getUserService();
+//        userService.addFriend("191543132", "191543107");
+//        userService.addFriend("191543132", "191543108");
+//        userService.addFriend("191543132", "191543109");
+        InformationService informationService = ServiceFactory.getInformationService();
+        System.out.println(informationService.queryAllInformation("191543132"));
     }
 
 }
