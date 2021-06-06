ae2a759dc4e04d7ae2879223475a26568dac3b0f
iamcht <605908700@qq.com>
2021-06-06 17:40:07 +0800

    修改了InformationServiceImpl

--- src/main/java/service/impl/InformationServiceImpl.java
+++ src/main/java/service/impl/InformationServiceImpl.java
@@ -1,18 +1,63 @@
 package service.impl;
 
+import commom.factory.DaoFactory;
+import dao.FriendRequestDao;
+import dao.PairingRequestDao;
+import dao.UserDao;
+import pojo.FriendRequest;
+import pojo.PairingRequest;
+import pojo.User;
 import service.InformationService;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class InformationServiceImpl implements InformationService {
 
+    private final PairingRequestDao pairingRequestDao;
+    private final List<PairingRequest> pairingRequestList;
+    private final FriendRequestDao friendRequestDao;
+    private final List<FriendRequest> friendRequestList;
+
+    public InformationServiceImpl(){
+        this.friendRequestDao = DaoFactory.getFriendRequestDao();
+        this.pairingRequestDao = DaoFactory.getPairingRequestDao();
+        this.friendRequestList = friendRequestDao.queryAllFriendRequest();
+        this.pairingRequestList = pairingRequestDao.queryAllPairingRequest();
+    }
+
     @Override
     public List<Object> queryAllInformation(String sno) {
-        return null;
+        List<Object> list = new LinkedList<>();
+        List<FriendRequest> p = new LinkedList<>(); //存放有人给我发的好友请求
+        List<PairingRequest> q = new LinkedList<>();//存放有人接受我的配对请求
+        for(FriendRequest i : friendRequestList){
+            if(i.getApplicant().equals(sno) && i.getStatus() == 2 && i.getStatus() == 3){   //有人响应了我给他发的好友请求
+                list.add(i);
+            }
+            if(i.getRequested().equals(sno)){   //有人给我发好友请求
+                p.add(i);
+            }
+        }
+        for(PairingRequest j : pairingRequestList){
+            if(j.getStudentNumber().equals(sno)) {  //有人接受了我的配对
+                q.add(j);
+            }
+        }
+        for(FriendRequest i : p) {
+            for(PairingRequest j : q) {
+                if(i.getApplicant().equals(j.getRecipientNumber()) && i.getRequested().equals(j.getStudentNumber())){
+                    q.remove(j);
+                }
+            }
+        }
+        list.add(q);
+        return list;
     }
 
     @Override
     public boolean removeInformationService(String ID, String type) {
+
         return false;
     }
 }
